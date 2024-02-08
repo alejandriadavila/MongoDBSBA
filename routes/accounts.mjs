b41bd84e301b07622613f2099ad322a9f2dd99ec
alejandriadavila <149810746+alejandriadavila@users.noreply.github.com>
2024-02-08 18:08:30 -0500

    Added get, post, patch, delete routes to accounts

--- routes/accounts.mjs
+++ routes/accounts.mjs
@@ -1,5 +1,4 @@
 import express from 'express'
-import { ObjectId } from 'mongodb'
 import accounts from '../models/accounts.mjs'
 
 const router = express.Router()
@@ -12,11 +11,46 @@ router.get("/", async(req, res) =>{
     })
 })
 
+// Create an account
+router.post("/", async(req, res) =>{
+    let query = {account_id: req.params.account_id}
+    let newDocument = req.body
+
+    let preexistingAccount = await accounts.find(query)
+    if (!preexistingAccount) res.send("Account number already in use.")
+    else{
+        if(newDocument.account_id && newDocument.limit){
+            let result = await accounts.insertOne(newDocument)
+            res.send(result).status(204)
+        }
+    }
+})
+
 // Find a single account
 router.get("/:id", async(req, res) =>{
     let foundAccount = await accounts.findById(req.params.id)
     res.status(200).json({
         data: foundAccount
+    })
+})
+
+// Add a product to an account
+router.patch("/:id/add", async(req, res) =>{
+    let query = {_id: req.params.id}
+
+    let foundAccount = await accounts.find(query)
+    let result = await accounts.findByIdAndUpdate(foundAccount[0]._id, {products: [...foundAccount[0].products, req.body]}, {new: true})
+
+    res.status(200).json({
+        data: result
+    })
+})
+
+// Delete a single account
+router.delete("/:id", async(req, res) =>{
+    await accounts.findByIdAndDelete(req.params.id)
+    res.status(204).json({
+        data: "Item was deleted"
     })
 })
 
